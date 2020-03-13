b925727e82ea7867be04a3b6deb7736cede9645b
Shing Chan <ymanhei@gmail.com>
2020-03-13 15:04:22 +0800

    first draft 154

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: '100mb' }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 //app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
